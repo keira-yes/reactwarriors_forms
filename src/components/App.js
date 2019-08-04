a6a302997a0f7a23100d4ec5e1acbf59fe4d1add
Kirillova <keira.kirillova@gmail.com>
2019-08-04 15:34:53 +0300

    add refs and state to input to get value

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,27 @@
 import React from "react";
 
 export default class App extends React.Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      name: ''
+    }
+  }
+
+  onSubmitForm = (e) => {
+    e.preventDefault();
+    console.log("refs", this.name.value);
+    console.log("submit", this.state)
+  };
+
+  onChangeName = (e) => {
+    this.setState({
+      name: e.target.value
+    })
+  };
+
   render() {
     return (
       <div className="form-container card">
@@ -11,6 +32,8 @@ export default class App extends React.Component {
               type="text"
               className="form-control"
               placeholder="Enter username"
+              ref={node => this.name = node}
+              onChange = {this.onChangeName}
             />
           </div>
           <div className="form-group">
@@ -29,7 +52,7 @@ export default class App extends React.Component {
               placeholder="Enter repeat password"
             />
           </div>
-          <button type="submit" className="btn btn-primary w-100">
+          <button type="submit" className="btn btn-primary w-100" onClick={this.onSubmitForm}>
             Submit
           </button>
         </form>
