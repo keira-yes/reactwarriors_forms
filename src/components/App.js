c9175183736f8e2ab78117a434bd51e4b5ea2f89
Kirillova <keira.kirillova@gmail.com>
2019-08-04 17:25:41 +0300

    refactoring of get value method, add select, add variable and method of displaying options of select

--- src/components/App.js
+++ src/components/App.js
@@ -1,62 +1,95 @@
 import React from "react";
+import countries from "../data/countries";
 
 export default class App extends React.Component {
 
-  constructor() {
-    super();
+    constructor() {
+        super();
 
-    this.state = {
-      name: ''
+        this.state = {
+            username: '',
+            password: '',
+            repeatPassword: '',
+            country: ''
+        }
     }
-  }
 
-  onSubmitForm = (e) => {
-    e.preventDefault();
-    console.log("refs", this.name.value);
-    console.log("submit", this.state)
-  };
+    onSubmitForm = (e) => {
+        e.preventDefault();
+        console.log("submit", this.state)
+    };
 
-  onChangeName = (e) => {
-    this.setState({
-      name: e.target.value
-    })
-  };
+    onChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    };
 
-  render() {
-    return (
-      <div className="form-container card">
-        <form className="form card-body">
-          <div className="form-group">
-            <label>Username</label>
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Enter username"
-              ref={node => this.name = node}
-              onChange = {this.onChangeName}
-            />
-          </div>
-          <div className="form-group">
-            <label>Password</label>
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Enter password"
-            />
-          </div>
-          <div className="form-group">
-            <label>Repeat password</label>
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Enter repeat password"
-            />
-          </div>
-          <button type="submit" className="btn btn-primary w-100" onClick={this.onSubmitForm}>
-            Submit
-          </button>
-        </form>
-      </div>
-    );
-  }
+    getItems = (items) => {
+        return items.map(item => {
+            return <option value={item.name} key={item.id}>{item.name}</option>
+        })
+    };
+
+    render() {
+
+        // const getCountry = countries.map(item => {
+        //     return <option value={item.name} key={item.id}>{item.name}</option>
+        // });
+
+        return (
+            <div className="form-container card">
+                <form className="form card-body">
+                    <div className="form-group">
+                        <label>Username</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            name="username"
+                            placeholder="Enter username"
+                            value={this.state.username}
+                            onChange = {this.onChange}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label>Password</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            name="password"
+                            placeholder="Enter password"
+                            value={this.state.password}
+                            onChange = {this.onChange}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label>Repeat password</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            name="repeatPassword"
+                            placeholder="Enter repeat password"
+                            value={this.state.repeatPassword}
+                            onChange = {this.onChange}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="countries">Select country</label>
+                        <select
+                            className="form-control"
+                            id="countries"
+                            name="country"
+                            value={this.state.country}
+                            onChange = {this.onChange}
+                        >
+                            {this.getItems(countries)}
+                        </select>
+                    </div>
+                    <button type="submit" className="btn btn-primary w-100" onClick={this.onSubmitForm}>
+                        Submit
+                    </button>
+                </form>
+            </div>
+        );
+    }
 }
